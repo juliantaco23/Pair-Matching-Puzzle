7b85aa3967ac38543b0b04222d1fbbfcfe64e62e
Juan Pablo Cano <canojuan0415@gmail.com>
2022-07-23 09:05:40 -0500

    Feat: First commit

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+const morgan = require('morgan');
+
+
+
+app.use(morgan('dev'));
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
