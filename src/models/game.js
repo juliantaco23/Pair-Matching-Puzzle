2f31ff3ddfed7d550fbaa180fa093433baaa2b97
Ian Marcos <ian.marcos.ortiz@gmail.com>
2022-07-23 10:52:03 -0500

    cardList set

--- src/models/game.js
+++ src/models/game.js
@@ -1,30 +1,56 @@
 const Helper = require('./helpers'); 
 
 class Game {
+  #id;
   #cardCount;
   #imagesList;
   #cardList;
-  // #formatCardList;
+  #attempts; 
   
   constructor(cardCount = 12) {
     if (typeof cardCount !== 'number' || cardCount % 2 !== 0) {
       cardCount = 12;
     }
     this.#cardCount = cardCount;
-    console.log(this.#cardCount)
-    this.#setImageList().then(() => this.#formatCardList);
+    this.#attemps = 0;
+    this.#setImageList()
+      .then(() => this.#setCardList());
   }
 
   async #setImageList() {
     const helper = new Helper();
     const response = await helper.fetchGameImages(this.#cardCount);
-    this.#imagesList = response.data;
+    this.#imagesList = response.data.map(img => {
+      const {id, download_url} = img;
+      return {id, urlImg: download_url};
+    })
+    return;
   }
 
-  #formatCardList() {
-    
+  #setCardList() {
+    const helper = new Helper();
+    this.#cardList = [...this.#imagesList, ...this.#imagesList];
+    helper.shuffleList(this.#cardList);
   }
 
+  #isMaxAttempt() {
+    if (this.#attempts >= this.#cardCount) {
+      return true;
+    }
+  }
+
+  get attempts() {
+    return this.#attempts;
+  }
+
+  addOneAttemp() {
+    return this.#attempts ++;
+    this.#isMaxAttempt()
+  }
+
+  get cardList() {
+    return this.#cardList;
+  }
 }
 
 module.exports = Game;
